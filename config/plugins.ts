409e9a1e069e273b58493d1e91893a152e2b7b3c
Jacob Logas <logasja@outlook.com>
2022-11-24 22:33:45 -0500

    Change plugins

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
-      provider: "strapi-provider-upload-ipfs",
+      provider: "strapi-provider-upload-ipfs-client",
       providerOptions: {
         grpc: env("IPFS_GRPC"),
         http: env("IPFS_API_HTTP"),
