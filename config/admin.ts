192c264703ca13a3bed2192a2043c2f197f00bf1
Jacob Logas <logasja@outlook.com>
2024-03-26 22:41:36 -0400

    Fix some stuffs

--- config/admin.ts
+++ config/admin.ts
@@ -1,13 +1,13 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'testsecret'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'testsalt'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'testsalt1'),
     },
   },
   flags: {
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-ipfs-client",
+      providerOptions: {
+        clientOptions: {
+          host: env('IPFS_HOST', 'localhost'),
+          port: env.int('IPFS_PORT', 5001),
+        },
+        gatewayUrl: env('IPFS_GATEWAY', '/ip4/127.0.0.1/tcp/8080/http')
+      }
+    }
+  }
+});
